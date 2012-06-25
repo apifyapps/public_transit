279f699e72f88276ff0472acc9fb737b8b1dba37
Sathish <sathish316@gmail.com>
2012-06-26 02:13:06 +0530

    display route

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,6 @@
 var routesUrl = {'Chennai': "http://localhost:9393/api/chennai_bus_routes.json?callback=?"}
 var city = 'Chennai';
+
 var routes = [];
 var stops = [];
 var adjacencyGraph = {};
@@ -9,43 +10,10 @@ $(function(){
   // Load routes
   $.getJSON(routesUrl[city], function(data){
     routes = JSON.parse(data);
-    stops = _.sortBy(_.union(_.flatten(_.map(routes, function(route){return [].concat(route.start, route.end, route.via)}))), function(stop){return stop});
-    buildAdjacencyGraph(stops, routes);
+    buildRoutesAndStops();
+    buildAdjacencyGraph();
     bindControls();
   });
-
-  function buildAdjacencyGraph(stops, routes){
-    adjacencyGraph = {};
-    sameStops = {};
-    for(var i=0; i< routes.length; i++){
-      var route = routes[i];
-      var prevStop = route.start;
-      addSameStop(prevStop, route.route);
-      if(route.via.length > 0){
-        for(var j=0; j < route.via.length; j++){
-          var routeAndStop = route.route + "|" + prevStop;
-          if(typeof(adjacencyGraph[routeAndStop]) == 'undefined') adjacencyGraph[routeAndStop] = [];
-          var nextStop = route.via[j];
-          addSameStop(nextStop, route.route);
-          var routeAndNextStop = route.route + "|" + nextStop;
-          adjacencyGraph[routeAndStop].push(routeAndNextStop);
-          prevStop = nextStop;
-        }
-      }
-      if(route.end.length > 0){
-        var routeAndStop = route.route + "|" + prevStop;
-        var routeAndNextStop = route.route + "|" + route.end;
-        addSameStop(route.end, route.route);
-        if(typeof(adjacencyGraph[routeAndStop]) == 'undefined') adjacencyGraph[routeAndStop] = [];
-        adjacencyGraph[routeAndStop].push(routeAndNextStop);
-      }
-    }
-  }
-
-  function addSameStop(stop, route){
-    if(typeof(sameStops[stop]) == 'undefined') sameStops[stop] = [];
-    sameStops[stop].push(route + "|" + stop);
-  }
 
   function bindControls(){
     $('#from').typeahead({
@@ -57,12 +25,102 @@ $(function(){
     $('#find-route').click(function(){
       var from = $('#from').val();
       var to = $('#to').val();
-      var path = findPath(from, to, adjacencyGraph);
+      var path = bfsShortestPath(from, to);
       return false;
     });
   }
 
-  function findPath(from, to, routes){
-    console.log("TODO: Find route " + from + " => " + to);
+  function buildRoutesAndStops(){
+    $('.city').html(city);
+    routes = _.map(routes, function(route){
+      route.via = route.via.split(/,\s*/);
+      return route;
+    });
+    stops = _.union(_.flatten(_.map(routes, function(route){
+      return [].concat(route.start, route.end, route.via);
+    })));
+  }
+
+  function buildAdjacencyGraph(){
+    adjacencyGraph = {};
+    sameStops = {};
+    for(var i=0; i< routes.length; i++){
+      var route = routes[i];
+      var prevStop = route.start;
+      addSameStop(prevStop, route.route);
+      if(route.via.length > 0){
+        for(var j=0; j < route.via.length; j++){
+          var routeAndStop = [route.route, prevStop];
+          if(typeof(adjacencyGraph[routeAndStop]) == 'undefined') adjacencyGraph[routeAndStop] = [];
+          var nextStop = route.via[j];
+          if(nextStop.length > 0){
+            addSameStop(nextStop, route.route);
+            var routeAndNextStop = [route.route, nextStop];
+            adjacencyGraph[routeAndStop].push(routeAndNextStop);
+            prevStop = nextStop;
+          }
+        }
+      }
+      if(route.end.length > 0){
+        var routeAndStop = [route.route, prevStop];
+        var routeAndNextStop = [route.route, route.end];
+        addSameStop(route.end, route.route);
+        if(typeof(adjacencyGraph[routeAndStop]) == 'undefined') adjacencyGraph[routeAndStop] = [];
+        adjacencyGraph[routeAndStop].push(routeAndNextStop);
+      }
+    }
+  }
+
+  function addSameStop(stop, route){
+    if(typeof(sameStops[stop]) == 'undefined') sameStops[stop] = [];
+    sameStops[stop].push([route, stop]);
+  }
+
+  function bfsShortestPath(startStop, endStop){
+    var startRouteStop = sameStops[startStop][0];
+    var explored = [startRouteStop];
+    var queue = [[startRouteStop]];
+    var count = 0;
+    while(queue.length > 0){
+      count += 1;
+      if(count > 10000)
+        noRouteFound();
+      var path = queue.shift();
+      var node = path[path.length-1];
+      if(node[1] == endStop){
+        console.log(path);
+        displayPath(path);
+        return path;
+      }
+      var adjRouteStops = adjacencyGraph[node] || [];
+      var adjRouteStops = adjRouteStops.concat(sameStops[node[1]]);
+      for(var i=0; i<adjRouteStops.length; i++){
+        adjacent = adjRouteStops[i];
+        if(!_.include(explored, adjacent)){
+          explored.push(adjacent);
+          var new_path = path.slice(0);
+          new_path.push(adjacent);
+          queue.push(new_path);
+
+        }
+      }
+    }
+  }
+
+  function displayPath(path){
+    //temp fix for algo
+    while(path[0] && path[1] && path[0][1] == path[1][1]){
+      path.shift();
+    }
+    $('.route_table').show();
+    $('.route_table .contents').html('');
+    for (var i=0;i<path.length;i++){
+      $('.route_table .contents').append('<tr><td>' + path[i][0]+ '</td><td>' + path[i][1] + '</td></tr>');
+    }
+  }
+
+  function noRouteFound(){
+    $('.route_table').show();
+    $('.route_table .contents').html("<td colspan='2'>No Route Found</td>");
   }
 });
