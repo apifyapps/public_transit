8cb72100df2e03ba988530852534937dec4f74c0
Sathish <sathish316@gmail.com>
2012-06-26 01:09:37 +0530

    shortest distance

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,20 +1,50 @@
-var routesUrl = "http://localhost:9393/api/chennai_bus_routes.json?callback=?"
+var routesUrl = {'Chennai': "http://localhost:9393/api/chennai_bus_routes.json?callback=?"}
 var city = 'Chennai';
 var routes = [];
 var stops = [];
 var adjacencyGraph = {};
+var sameStops = {};
 
 $(function(){
   // Load routes
-  $.getJSON(routesUrl, function(data){
+  $.getJSON(routesUrl[city], function(data){
     routes = JSON.parse(data);
     stops = _.sortBy(_.union(_.flatten(_.map(routes, function(route){return [].concat(route.start, route.end, route.via)}))), function(stop){return stop});
-    adjacencyGraph = buildAdjacencyGraph(stops, routes);
+    buildAdjacencyGraph(stops, routes);
     bindControls();
   });
 
   function buildAdjacencyGraph(stops, routes){
+    adjacencyGraph = {};
+    sameStops = {};
+    for(var i=0; i< routes.length; i++){
+      var route = routes[i];
+      var prevStop = route.start;
+      addSameStop(prevStop, route.route);
+      if(route.via.length > 0){
+        for(var j=0; j < route.via.length; j++){
+          var routeAndStop = route.route + "|" + prevStop;
+          if(typeof(adjacencyGraph[routeAndStop]) == 'undefined') adjacencyGraph[routeAndStop] = [];
+          var nextStop = route.via[j];
+          addSameStop(nextStop, route.route);
+          var routeAndNextStop = route.route + "|" + nextStop;
+          adjacencyGraph[routeAndStop].push(routeAndNextStop);
+          prevStop = nextStop;
+        }
+      }
+      if(route.end.length > 0){
+        var routeAndStop = route.route + "|" + prevStop;
+        var routeAndNextStop = route.route + "|" + route.end;
+        addSameStop(route.end, route.route);
+        if(typeof(adjacencyGraph[routeAndStop]) == 'undefined') adjacencyGraph[routeAndStop] = [];
+        adjacencyGraph[routeAndStop].push(routeAndNextStop);
+      }
+    }
+  }
 
+  function addSameStop(stop, route){
+    if(typeof(sameStops[stop]) == 'undefined') sameStops[stop] = [];
+    sameStops[stop].push(route + "|" + stop);
   }
 
   function bindControls(){
